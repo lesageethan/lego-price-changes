2213dc3d2c72e93dad0c56ced93d6c4ef482a492
lesageethan <lesageethan@gmail.com>
2024-01-16 22:18:03 +0000

    Added more accurate retirement delta calculator

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pandas as pd 
 import numpy as np
+from datetime import datetime
+import warnings
+warnings.filterwarnings("ignore")
 
 # Read excel into dataframe
 df = pd.read_excel("original-data/DiB whole sample Dec 2015 prices.xlsx")
@@ -9,19 +12,44 @@ df['Profit Margin'] = (
     df['Secondary market prices of new sets in 2015'] / df['Primary market price at release']
 )
 
-# Create years since release column
-df['Years Since Release'] = (
-    2016 - df['year of release']
+# Get accurate retirement dates
+df_retirement_list = []
+for i in range(1981, 2016):
+    df_retirement_list.append(pd.read_csv(str("original-data/"+str(i)+".csv")))
+
+df_retirement = pd.concat(df_retirement_list, ignore_index=True)
+
+print(df_retirement)
+print(df)
+
+df['Full Retirement Date'] = (
+    ""
 )
+df['Years Since Retirement'] = (
+    ""
+)
+
+for i in range(0, len(df['id'])):
+    try:
+        retirement_date = df_retirement.loc[df_retirement['number']==df['id'][i], 'US_dateLastAvailable'].iloc[0]
+        years_since_retirement = datetime.strptime("2016-01-01T00:00:00Z", "%Y-%m-%dT%H:%M:%SZ") -  datetime.strptime(retirement_date, "%Y-%m-%dT%H:%M:%SZ")
+        years_since_retirement = years_since_retirement.days / 365
+    except:
+        retirement_date = float('nan')
+        years_since_retirement = float('nan')
+
+    df['Full Retirement Date'][i] = retirement_date
+    df['Years Since Retirement'][i] = years_since_retirement
 
 # Create yearly average returns column
 df['Yearly Average Returns'] = (
-    df['Profit Margin']**(1/df['Years Since Release'])
+    np.where(df['Years Since Retirement']>2, df['Profit Margin']**(1/df['Years Since Retirement']), float('nan'))
 )
 
 # Find the average yearly return of all sets
 df = df.replace([np.inf, -np.inf], np.nan)
 print(df['Yearly Average Returns'].dropna().sum() / len(df['Yearly Average Returns'].dropna()))
+print(len(df['Yearly Average Returns'].dropna()))
 
 # Save CSV
 df.to_csv('new-data/dataframe_appended.csv')
