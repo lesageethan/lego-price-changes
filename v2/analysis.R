4a2928370fc29b0c09bccee02ac320518ee3d29a
lesageethan <lesageethan@gmail.com>
2024-02-15 18:17:17 +0000

    finalised dataset, added correlation analysis

--- v2/analysis.R
+++ v2/analysis.R
@@ -1,5 +1,6 @@
 library(ggplot2)
 library(dplyr)
+library(ggcorrplot)
 
 setwd('C:/Users/lesag/OneDrive - Cardiff University/Desktop/Github/lego-price-changes/v2')
 
@@ -10,10 +11,13 @@ head(df)
 final_date = as.Date(paste0('01 ', 'January 2024'), format = '%d %B %Y')
 
 df <- df %>% 
-  filter(retail>100) %>%
+  filter(retail>0) %>% 
+  filter(num_parts>=00) %>%
+  filter(rating>=0) %>%
   mutate(total_returns = value/retail) %>%
   mutate(retire_date_object = as.Date(paste0('01 ', retire_month), format = '%d %B %Y')) %>%
-  mutate(years_since_retirement = as.numeric(difftime(final_date, retire_date_object, units = 'days'))/365) %>%
+  mutate(years_since_retirement = as.numeric(difftime(final_date, retire_date_object, units = 'days'))/365) %>% 
+  filter(years_since_retirement>1) %>%
   mutate(cagr = total_returns**(1/years_since_retirement))
 
 mean(df$cagr)
@@ -21,7 +25,16 @@ length(df$cagr)
 sd(df$cagr)
 
 df %>% ggplot() +
-  geom_point(aes(x=retail, y=cagr))
+  geom_point(aes(x=rating, y=cagr))
 
-df %>% ggplot() +
-  geom_density(aes(x=cagr))
+df %>% 
+  ggplot() +
+  geom_boxplot(aes(group=rating, x=rating, y=cagr))
+
+
+df_nums <- df %>% select(where(is.numeric))
+corr <- round(cor(df_nums), 1)
+ggcorrplot(corr)
+
+# df %>% ggplot() +
+#   geom_density(aes(x=cagr))
